55720ae6e18c146f9d366fab4f8554b0173f9bbd
Tina <longevity.tina@gmail.com>
2020-05-24 16:22:35 -0700

    Alligns footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,28 @@
 import React from 'react'
-import { FaLinkedinIn } from '@fortawesome/react-fontawesome'
 import styles from "./Footer.module.css"
-
+import '../../src/fontawesome'
 
 const Footer = () => (
-  <ul className="icons">
+  <div>
+    <ul style={{ listStyle: `none`, float: `left` }}>
+      <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+        <a href="https://www.linkedin.com/in/tina-taylor-codes/" className="fab fa-linkedin">
+          <span className="label">LinkedIn</span>
+        </a>
+      </li>
+      <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+        <a href="https://github.com/longevitytina" className="icon fa-github">
+          <span className="label">Github</span>
+        </a>
+      </li>
 
-    <li>
-      <a href="https://www.linkedin.com/in/tina-taylor-codes/" className="FaLinkedinIn">
-        <span className="label">LinkedIn</span>
-      </a>
-    </li>
-    <li>
-      <a href="https://github.com/longevitytina" className="icon fa-github">
-        <span className="label">Github</span>
-      </a>
-    </li>
-
-    <li>
-      <a href="#" className="icon fa-envelope-o">
-        <span className="label">Email</span>
-      </a>
-    </li>
-  </ul>
+      <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+        <a href="#" className="icon fa-envelope-o">
+          <span className="label">Email</span>
+        </a>
+      </li>
+    </ul>
+  </div>
 )
 
 export default Footer
