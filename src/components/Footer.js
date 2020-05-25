a698b11b09c028d879f8697deead09903f8e5d5d
Tina <longevity.tina@gmail.com>
2020-05-25 09:27:55 -0700

    imports icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React from "react"
 import styles from "./Footer.module.css"
-import '../../src/fontawesome'
+import "../../src/fontawesome"
+import { FaLinkedinIn } from "react-icons/fa"
+import { FaGithub } from "react-icons/fa"
 
 const Footer = () => (
   <div>
     <ul style={{ listStyle: `none`, float: `left` }}>
       <li style={{ display: `inline-block`, marginRight: `1rem` }}>
-        <a href="https://www.linkedin.com/in/tina-taylor-codes/" className="fab fa-linkedin">
-          <span className="label">LinkedIn</span>
+        <a
+          href="https://www.linkedin.com/in/tina-taylor-codes/"
+          className="fab fa-linkedin"
+        >
+          <span className="label">
+            <FaLinkedinIn />
+          </span>
         </a>
       </li>
       <li style={{ display: `inline-block`, marginRight: `1rem` }}>
         <a href="https://github.com/longevitytina" className="icon fa-github">
-          <span className="label">Github</span>
+          <span className="label">
+            <FaGithub />
+          </span>
         </a>
       </li>
 
