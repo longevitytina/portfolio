bf7d1bdca964fd6fdf2d0bdf493f0b7eae7d838d
Tina <longevity.tina@gmail.com>
2020-05-25 20:13:49 -0700

    Major layout edit for project itm grid

--- src/components/Projects/components/Projects.js
+++ src/components/Projects/components/Projects.js
@@ -1,20 +1,29 @@
-import React from 'react'
-import ProjectItem from './ProjectItem'
-import { DEFAULT_PROJECTS } from '../constants/projectPreviews'
+import React from "react"
+import ProjectItem from "./ProjectItem"
+import { DEFAULT_PROJECTS } from "../constants/projectPreviews"
 
 const projects = DEFAULT_PROJECTS
 
 export default function Projects() {
-    return (
-        <div>
-            <h2>All Projects</h2>
-            {projects.map((project) => (
-                <ProjectItem
-                    {...project}
-                />
-
-            ))
-            }
-        </div>
-    )
+  return (
+    <div style={{ marginTop: `80px`, marginLeft: `30px` }}>
+      <h2>Recent Projects</h2>
+      <div
+        className="tiles"
+        style={{
+          display: `flex`,
+          flexDirection: `row`,
+          flexWrap: `wrap`,
+          justifyContent: `space-evenly`,
+          margin: `10px`,
+        }}
+      >
+        {projects.map(project => (
+          <>
+            <ProjectItem {...project} />
+          </>
+        ))}
+      </div>
+    </div>
+  )
 }
