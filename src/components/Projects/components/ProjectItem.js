5c22fbc5f537b11c0076cb11a670463c1609a81b
Tina <longevity.tina@gmail.com>
2020-05-26 09:50:13 -0700

    Shadow styling

--- src/components/Projects/components/ProjectItem.js
+++ src/components/Projects/components/ProjectItem.js
@@ -4,8 +4,19 @@ import { Button } from "react-bootstrap"
 
 export default function ProjectItem({ source, link }) {
   return (
-    <div style={{ position: `relative` }}>
-      <img src={source} className={styling.image} alt="" />
+    <div
+      style={{
+        position: `relative`,
+      }}
+    >
+      <img
+        src={source}
+        className={styling.image}
+        alt=""
+        style={{
+          boxShadow: `0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)`,
+        }}
+      />
       <Button
         variant="outline-secondary"
         size="sm"
