90f630f50fd3495ef55b244caa995c1e37c351b3
Tina <longevity.tina@gmail.com>
2020-05-25 20:11:56 -0700

    Updated projects

--- src/components/Projects/constants/projectPreviews.js
+++ src/components/Projects/constants/projectPreviews.js
@@ -1,16 +1,21 @@
-import wayfarer from '../../../assets/images/projectPreview/Wayfarer.png'
-import massageify from '../../../assets/images/projectPreview/Massageify.png'
+import wayfarer from "../../../assets/images/projectPreview/Wayfarer.png"
+import massageify from "../../../assets/images/projectPreview/Massageify.png"
+import cvwellness from "../../../assets/images/projectPreview/CVWellness.png"
 
 export const DEFAULT_PROJECTS = [
-    {
-        id: '1',
-        source: wayfarer,
-
-    },
-    {
-        id: '2',
-        source: massageify,
-
-    },
-
+  {
+    id: "1",
+    source: wayfarer,
+    link: "https://sheltered-thicket-24218.herokuapp.com/",
+  },
+  {
+    id: "2",
+    source: massageify,
+    link: "https://massage-ify.herokuapp.com/",
+  },
+  {
+    id: "3",
+    source: cvwellness,
+    link: "https://guarded-stream-94756.herokuapp.com/",
+  },
 ]
