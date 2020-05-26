2e413c90d8a947b402a8d53bf12290c486b0e758
Tina <longevity.tina@gmail.com>
2020-05-25 20:13:16 -0700

    Adds button to project item

--- src/components/Projects/components/ProjectItem.js
+++ src/components/Projects/components/ProjectItem.js
@@ -1,10 +1,24 @@
-import React from 'react'
-import styling from './ProjectItem.module.css'
+import React from "react"
+import styling from "./ProjectItem.module.css"
+import { Button } from "react-bootstrap"
 
-export default function ProjectItem({ source }) {
-    return (
-        <div>
-            <img src={source} className={styling.image} alt="" />
-        </div>
-    )
+export default function ProjectItem({ source, link }) {
+  return (
+    <div style={{ position: `relative` }}>
+      <img src={source} className={styling.image} alt="" />
+      <Button
+        variant="outline-secondary"
+        size="sm"
+        style={{
+          position: `absolute`,
+          top: `50%`,
+          left: `35%`,
+        }}
+        href={link}
+        target="_blank"
+      >
+        View Project
+      </Button>
+    </div>
+  )
 }
